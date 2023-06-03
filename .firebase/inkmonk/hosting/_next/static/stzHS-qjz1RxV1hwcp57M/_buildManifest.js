a7466883b7fbd1e23f961410d6e42db961a4f0e6
MethIngestor <moonywlk@gmail.com>
2023-06-03 17:44:30 +0800

    first commit

---
+++ .firebase/inkmonk/hosting/_next/static/stzHS-qjz1RxV1hwcp57M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-e22eb3c9b1d9818c.js","static/chunks/pages/index-84ae31495840ce61.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
